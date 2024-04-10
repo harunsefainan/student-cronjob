c03d62fbe87044eb3e32fe7df3a1c33ce350f38d
harunsefainan <harunsefa.inan@gmail.com>
2024-04-11 01:04:42 +0300

    update

--- src/main/java/com/harunsefainan/studentcronjob/repositories/IStudentCronjobRepository.java
+++ src/main/java/com/harunsefainan/studentcronjob/repositories/IStudentCronjobRepository.java
@@ -2,6 +2,8 @@ package com.harunsefainan.studentcronjob.repositories;
 
 import com.harunsefainan.studentcronjob.entities.StudentCronjobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IStudentCronjobRepository extends JpaRepository<StudentCronjobEntity, String> {
 }
