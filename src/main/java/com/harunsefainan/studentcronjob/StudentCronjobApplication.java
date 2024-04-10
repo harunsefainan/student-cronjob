0d44e9e6860d027782c89ff4feaa5a23ad3afe0b
harunsefainan <harunsefa.inan@gmail.com>
2024-04-10 15:56:02 +0300

    second commit

---
+++ src/main/java/com/harunsefainan/studentcronjob/StudentCronjobApplication.java
@@ -0,0 +1,13 @@
+package com.harunsefainan.studentcronjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCronjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCronjobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harunsefainan/studentcronjob/StudentCronjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harunsefainan.studentcronjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCronjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
